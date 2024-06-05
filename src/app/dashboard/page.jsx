616078a3000bd6a125655dc6769f4215eb809b38
winnyDiese <winnydiese@gmail.com>
2024-06-05 16:27:05 +0200

    luvluyvkuyv

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,14 @@
 
 
+import { redirect } from 'next/navigation'
+import { useSession, signOut } from "next-auth/react";
 import React from 'react'
 
 const Dashboard = () => {
+  const {data:session,status} = useSession()
+
+  if(!session) redirect('/login')
+
   return (
     <div className="p-20">
         <p className='text-3xl'>Dashboard</p>
