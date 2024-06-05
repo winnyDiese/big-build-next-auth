9c0068095af90c4515fc38412fb155b64ee08da6
winnyDiese <winnydiese@gmail.com>
2024-06-05 16:33:34 +0200

    livkuyvk

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,4 +1,5 @@
 
+'use client'
 
 import { redirect } from 'next/navigation'
 import { useSession, signOut } from "next-auth/react";
@@ -8,10 +9,16 @@ const Dashboard = () => {
   const {data:session,status} = useSession()
 
   if(!session) redirect('/login')
+  console.log(session)
+
+  const handleLogout = ()=>{ signOut() }
 
   return (
     <div className="p-20">
-        <p className='text-3xl'>Dashboard</p>
+        <p className='text-3xl mb-6'>Dashboard</p>
+        <p className='text-sm'>{session?.user?.name}</p>
+
+        <button onClick={handleLogout} className="bg-black p-3 text-white"> Logout </button>
     </div>
   )
 }
