58d5b79d3209a359654ff8220aa57dd4de105c04
winnyDiese <winnydiese@gmail.com>
2024-06-05 15:38:09 +0200

    ljhvkuyvuy

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -11,9 +11,10 @@ const page = () => {
             <form action="" onSubmit={handleSubmit}>
                 <div className='font-bold mb-3'>Login</div> <br/>
 
-                <input type="text" placeholder='email' className='p-2 border bg-slate-200 mb-4' /> <br/>
-                <input type="password" placeholder='password' className='p-2 border bg-slate-200 mb-4'  /> <br/>
+                <input value={data.email} onChange={e=>setData({...data,email:e.target.value})} type="email" placeholder='email' className='p-2 border bg-slate-200 mb-4' /> <br/>
+                <input value={data.password} onChange={e=>setData({...data,password:e.target.value})} type="password" placeholder='password' className='p-2 border bg-slate-200 mb-4'  /> <br/>
                 <button type="submit" className='px-5 bg-purple-300 '>Login</button>
+                
             </form>
 
         </div>
