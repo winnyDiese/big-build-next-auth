69ccbf5afe3ffa88b729e5ebfe71e22ad82f6d37
winnyDiese <winnydiese@gmail.com>
2024-06-05 13:35:24 +0200

    lhvivihv

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,1 +1,20 @@
 
+
+import User from "../../../../models/user"
+import { NextResponse } from "next/server"
+import connectDb from "../../../../lib/mongoose"
+
+await connectDb()
+
+export async function GET(){
+    const users = await User.find()
+    return NextResponse.json({message:"Users",users},{status:201})
+}
+
+export async function POST(request){
+    const {names,email,password} = await request.json()
+    const user = await User.create({names,email,password})
+
+    return NextResponse.json({message:"User added !",user},{status:201})
+}
+
